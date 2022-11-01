419d100d97cfd27041022218615e478769d1f27d
Berwyn <berhoyt@gmail.com>
2022-11-02 11:53:56 +1300

    Improved to select a preset suited to the DVD resolution

--- rip.py
+++ rip.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python3
 
-""" Usage: python rip.py <source_file> [output_path] """
+""" Usage: python rip.py [-d] <source_file> [output_path]
+  where -d means dry-run (i.e. show what it would have done)
+"""
 
 #edit the following if you wish to change them
 handbrake = "HandBrakeCLI"
-minimum_seconds = 60*3
+minimum_seconds = 60*1
 
-# Logfiles will go in the output_path
+# Logfiles will go in the Output_path
 logfile = 'rip.log'
 handbrakelog = 'handbrake.log'
+
+# The following are added after the appropriate preset (i.e. 'HQ 480p30 Surround' -- see table below)
+Handbrake_options = ['--optimize']  # web optimize (directory at start of file)
+
+# table of DVD sizes mapping to the appropriate preset
+presets = {
+    480: 'HQ 480p30 Surround',
+    576: 'HQ 576p25 Surround',
+    720: 'HQ 720p30 Surround',
+    1080: 'HQ 1080p30 Surround',
+}
 
 import sys
 import os
 import subprocess
 import re
-import collections
+from types import SimpleNamespace
 import glob
 import datetime
 
-output_path = ''
+Output_path = ''
 
 def log(string):
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S ')
@@ -46,75 +59,91 @@ def capture_shell(*args):
     return process_info
 
 def scan(source):
-    """ Find titles in input source and return a dict of titles with fields '1', '2', '3', ...
+    """ Find numbered titles in input source and return a dict of titles with fields 1, 2, 3, ...
         with each field's value a duration object with fields 'seconds' and 'text' (for human-readable time).
-        If there is a line "DVD Title: <name>" in the source scan, include it as a field 'title' in the dict.
+        If there is a line "DVD Title: <name>" in the source scan, include it as a field 'dvdtitle' in the dict.
     """
     lines = capture_shell(handbrake, '-t0', '-i', source).stderr
-    Duration = collections.namedtuple('duration', 'seconds text')
     titles = {}
     for line in lines:
         istitle = re.search(r"\+ title ([0-9]+):", line)
         if istitle:
-            title = istitle[1]
+            title = int(istitle[1])
+            titles[title] = SimpleNamespace(title=title)
         isduration = re.search(r"\+ duration: (([0-9]{2}):([0-9]{2}):([0-9]{2}))", line)
         if isduration:
             duration_text = isduration[1]
             hh, mm, ss = int(isduration[2]), int(isduration[3]), int(isduration[4])
             duration_seconds = hh*3600 + mm*60 + ss
-            titles[title] = Duration(duration_seconds, duration_text)
+            titles[title].seconds, titles[title].text = duration_seconds, duration_text
+        issize = re.search(r"\+ size: [0-9]+x([0-9]+)", line)
+        if issize:
+            titles[title].size = int(issize[1])
         isdvdtitle = re.search(r"DVD Title: (.*)", line)
         if isdvdtitle:
-            titles['title'] = isdvdtitle[1]
+            titles['dvdtitle'] = isdvdtitle[1]
     return titles
 
-def rip(source, dest, minimum_seconds=60*3):
-    """ Find titles in input source and rip them if they are longer than seconds duration """
+def rip(source, dest, minimum_seconds=0, dry_run=False):
+    """ Find titles in input source and rip them to "<dest>-<title>.m4v"
+        but only if they are longer than seconds duration
+        If dry_run is True, don't actually do the rip; just show what it would do
+    """
     titles = scan(source)
     if not titles:
         printlog(f"ERROR: No titles found in {source} -- skipping")
         return
 
     included = []
-    for title, duration in titles.items():
-        include = duration.seconds >= minimum_seconds
-        print(f"Title {title} ({duration.text})" + ("" if include else f" -- skipped because < {minimum_seconds}s"))
+    for title in titles.values():
+        include = title.seconds >= minimum_seconds
+        print(f"Title {title} ({title.text})" + ("" if include else f" -- skipped because < {minimum_seconds}s"))
         if include: included.append(title)
 
-    included.sort(key=lambda title: titles[title].seconds, reverse=True)
+    included.sort(key=lambda title: title.seconds)
     print("\nSmallest titles first:")
     for title in included:
-        duration = titles[title]
-        dest += f"-{int(title):02}.m4v"
-        printlog(f"Ripping title {int(title):02} ({duration.text}) as {dest}")
-        args = [handbrake, f"-t{title}", '--markers', '--optimize', '-i', source, '-o', dest]
+        outfile = dest + f"-{title.seconds//60:03}m-{title.title:02}.m4v"
+        printlog(f"Ripping title {title.title:02} ({title.text}) as '{outfile}'")
+        args = [handbrake, f"-t{title.title}", '-Z', presets[title.size], *Handbrake_options, '-i', source, '-o', outfile]
         args.append(f"2>>{handbrakelog}")
         timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S ')
         with open(handbrakelog, 'a') as hlog:
             print('\n' + timestamp + ' '.join(args), file=hlog)
-        process_info = subprocess.run(args, shell=True)
+        if dry_run:
+            print('  ' + ' '.join(args))
+        else:
+            process_info = subprocess.run(args, shell=True)
 
-def glob_rip(source, minimum_seconds=60*3):
-    """ Expand wildcards in source and rip() all matching files """
+def glob_rip(source, minimum_seconds=0, dry_run=False, output_path=None):
+    """ Expand wildcards in source and rip() all matching files
+        If dry_run is True, don't actually do the rip; just show what it would do
+    """
     for filename in glob.glob(source):
         basename, ext = os.path.splitext(filename)
-        dest = basename
-        if output_path and not filename[0] not in r"\/":
-            dest = os.path.join(output_path, filename)
-
+        directory = basename
+        if output_path and filename[0] not in r"\/":
+            directory = os.path.join(output_path, directory)
+        os.makedirs(directory, exist_ok=True)
+        dest = os.path.join(directory, basename)
         printlog(f"Ripping {filename}")
-        rip(filename, dest, minimum_seconds)
+        rip(filename, dest, minimum_seconds=minimum_seconds, dry_run=dry_run)
         print()
 
 
 if __name__ == "__main__":
+    dry_run = False
+    if '-d' in sys.argv:
+        sys.argv.remove('-d')
+        dry_run = True
+
     source = sys.argv[1]
     if len(sys.argv) > 2:
-        output_path = sys.argv[2]
-        logfile = os.path.join(output_path, logfile)
-        handbrakelog = os.path.join(output_path, handbrakelog)
+        Output_path = sys.argv[2]
+        logfile = os.path.join(Output_path, logfile)
+        handbrakelog = os.path.join(Output_path, handbrakelog)
 
     try:
-        glob_rip(source, minimum_seconds)
+        glob_rip(source, minimum_seconds, output_path=Output_path, dry_run=dry_run)
     except KeyboardInterrupt:
         print("User interrupted process", file=sys.stderr)
